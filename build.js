805ce91b7c1a8bda9e3f4cc053bd550f1c0d3b5d
Scott Weaver <sweaver2112@gmail.com>
2021-04-30 00:16:56 -0700

    added padding function for hex codes, /u32 => /u0032

--- build.js
+++ build.js
@@ -16,6 +16,10 @@ async function buildPattern(){
     let htm = await fetchHTML()
     let reg = new RegExp("(?<=<td class='chars'>).*?(?=</td>)", "g")
     let matches = htm.matchAll(reg);
-    let regex = "(?:"+[...matches].map(e=>e[0].split('').map(s=>String.raw`\u${s.codePointAt(0).toString(16)}`).join("")).sort((a,b)=>b.length-a.length).join("|")+")"
+    let regex = "(?:"+[...matches].map(e=>e[0].split('').map(s=>String.raw`\u${padHex(s.codePointAt(0).toString(16))}`).join("")).sort((a,b)=>b.length-a.length).join("|")+")"
     return regex;
+}
+function padHex(hex){
+ while(hex.length<4) hex = "0"+hex
+ return hex;
 }
